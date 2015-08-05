51965ec5747e2ee53e8583b18335310da82f442f
test <test@test-PC>
2015-08-05 16:39:36 +0530

    overloaded method

---
+++ Usingant/src/com/training/Greeting.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class Greeting {
+	public String getMessage(String name){
+		return name+"welcome";
+	}
+	public String getMessage()
+	{
+		System.out.println("added newly");
+	    return "Welcome to my ant program";
+	}
+
+}
